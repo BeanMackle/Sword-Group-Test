fb36d13b3ea8cd229ca1e87139f6525019a6bc58
gavin <gavin.mackle97@gmail.com>
2023-03-14 20:23:58 +0000

    Add project files.

---
+++ Sword Group Test/ArgumentHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sword_Group_Test
+{
+    internal class ArgumentHelpers
+    {
+        public static string[] ParseFile(string filePath) {
+			try
+			{			
+				return File.ReadAllLines(filePath);
+			}
+			catch (Exception e)
+			{
+				string error = $"Failed to parse file: {e.Message}";
+				Console.WriteLine(error);
+				return new string[0];
+			}
+        }
+
+		public static bool GetCaseSensitiveValue(string caseSensitiveString) {
+
+			if (bool.TryParse(caseSensitiveString, out bool caseSensitive))
+			{
+				return caseSensitive;
+			}
+			else {
+				Console.WriteLine("Failed to parse case sensitive argument. Setting to default of true.");
+				return true;
+			}
+        }
+    }
+}
---
+++ Sword Group Test/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Sword_Group_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args is null || !args.Any()) {
+                Console.WriteLine("Mandatory file location paramater not provided! Stopping process.....");
+                Environment.Exit(0);
+            }
+
+            string filePath = args[0];
+            string[] lines = ArgumentHelpers.ParseFile(filePath);
+
+            if (!lines.Any()) {
+                Console.WriteLine("Stopping process.....");
+                Environment.Exit(0);
+            }
+
+            bool caseSensitive = args.Length > 1 ? ArgumentHelpers.GetCaseSensitiveValue(args[1]) : true;
+
+            var charCountDictionary = BuildCharDictionary(lines, caseSensitive);
+            PrintResults(charCountDictionary);
+            Console.ReadKey();
+        }
+
+        static void PrintResults(Dictionary<char, int> charCountDictionary)
+        {
+            int totalCharacters = 0;
+            foreach(int count in charCountDictionary.Values)
+            {
+                totalCharacters += count;
+            }
+
+            Console.WriteLine($"Total Characters: {totalCharacters}");
+            var topCharacters = charCountDictionary.OrderByDescending(c => c.Value).Take(10).ToDictionary(d => d.Key, d=> d.Value);
+            foreach(char character in topCharacters.Keys)
+            {
+                Console.WriteLine($"{character} ({topCharacters.GetValueOrDefault(character)})");
+            }
+        }
+
+        static Dictionary<char, int> BuildCharDictionary(string[] lines, bool caseSensitive) {
+            Dictionary<char, int> charCountDictionary = new Dictionary<char, int>();
+
+            foreach (string line in lines)
+            {
+                string cleanLine = Regex.Replace(line, @"\p{C}+", String.Empty);
+                cleanLine = cleanLine.Replace(" ", String.Empty);
+                if(!caseSensitive)
+                {
+                    cleanLine = cleanLine.ToLower();
+                }
+
+                char[] chars = cleanLine.ToCharArray();
+
+                foreach(char character in chars) {
+                   if(charCountDictionary.TryGetValue(character, out int count))
+                    {
+                        charCountDictionary[character] = count + 1;
+                    }
+                    else
+                    {
+                        charCountDictionary.Add(character, 1);
+                    }
+                }
+            }
+            return charCountDictionary;
+        }
+    }
+}
